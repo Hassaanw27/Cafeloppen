528e15b35a9e01d7bae77b7b02d2cd4bdd162bae
Hassaanw27 <hassaanw27@gmail.com>
2020-05-25 12:34:49 +0200

    checkbox works without scrolling to top

--- Forside/script.js
+++ Forside/script.js
@@ -1,7 +1,21 @@
-$("#navtoggle").change(function () {
-  if ($(this).is(":checked")) {
-    $(".navbg").addClass("red");
-  } else {
-    $(".navbg").removeClass("red");
-  }
-});
+
+
+// $("#navtoggle").change(function () {
+//  if ($(this).is(":checked")) {
+//    $(".navbg").addClass("red");
+//  } else {
+//    $(".navbg").removeClass("red");
+//  }
+//});
+
+//$(window).scroll(function() {
+
+//    if ($(this).scrollTop()>-1)
+//     {
+//    $(".navbg").addClass("red");
+//     }
+//    else
+//     {
+//    $(".navbg").removeClass("red");
+//     }
+// });
