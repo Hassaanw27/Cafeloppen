2da2d991b4164efaa8ba2e2eb9d4bf148ce45bea
Hassaanw27 <hassaanw27@gmail.com>
2020-05-27 17:04:13 +0200

    Scrollhighlight works but is a little janky. Should ask ludvig how to

--- Forside/js/script.js
+++ Forside/js/script.js
@@ -14,6 +14,49 @@ $widget.waypoint(function (direction) {
   }
 },{offset:'92%'});
 
+//new//
+
+$(document).ready(function() {
+  $(document).on("scroll", onScroll);
+
+  //smoothscroll
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function() {
+      $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+
+    var target = this.hash,
+      menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top - 200
+    }, 500, 'swing', function() {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
+    });
+  });
+});
+
+function onScroll(event) {
+  var scrollPos = $(document).scrollTop();
+  $('#categories a').each(function() {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top - 210 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('#categories a').removeClass("active");
+      currLink.addClass("active");
+    } else {
+      currLink.removeClass("active");
+    }
+  });
+}
+
+
+
 // $("#navtoggle").change(function () {
 //  if ($(this).is(":checked")) {
 //    $(".navbg").addClass("red");
